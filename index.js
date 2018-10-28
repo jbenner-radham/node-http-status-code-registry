18e127b59d8a2e6304f0c54c276c4b8b514cf9e5
James Benner <james.benner@gmail.com>
2018-10-28 14:30:35 -0400

    Updated variable name.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const httpStatusCodeRegistry = require('./lib');
 (async () => {
     try {
         console.log(JSON.stringify(await httpStatusCodeRegistry(), null, 4));
-    } catch (e) {
-        console.error(e);
+    } catch (error) {
+        console.error(error);
     }
 })();
